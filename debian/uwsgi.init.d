26f850a2d90c342d9f9d14bc2aa7d2ff8ac1799c
Janos Guljas <janos@resenje.org>
2012-01-02 03:35:56 +0100

    Fix LSB Description lines.

--- debian/uwsgi.init.d
+++ debian/uwsgi.init.d
@@ -8,9 +8,9 @@
 # Short-Description: Start/stop uWSGI server instance(s)
 # Description:       This script manages uWSGI server instance(s).
 #                    You could control specific instance(s) by issuing:
-#
+#                    
 #                        service uwsgi <command> <confname> <confname> ...
-#
+#                    
 #                    You can issue to init.d script following commands:
 #                      * start        | starts daemon
 #                      * stop         | stops daemon
@@ -18,17 +18,17 @@
 #                      * force-reload | sends to daemon SIGTERM signal
 #                      * restart      | issues 'stop', then 'start' commands
 #                      * status       | shows status of daemon instance
-#
+#                    
 #                    'status' command must be issued with exactly one
 #                    argument: '<confname>'.
-#
+#                    
 #                    In init.d script output:
 #                      * . -- command was executed without problems or instance
 #                             is already in needed state
 #                      * ! -- command failed (or executed with some problems)
 #                      * ? -- configuration file for this instance isn't found
 #                             and this instance is ignored
-#
+#                    
 #                    For more details see /usr/share/doc/uwsgi/README.Debian.
 ### END INIT INFO
 
